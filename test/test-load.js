7810ac50639fb31594d10d6944e494042f05d89e
Steve Britton <stvnbritton@gmail.com>
2014-03-31 12:35:37 -0400

    updated generator name

--- test/test-load.js
+++ test/test-load.js
@@ -2,7 +2,7 @@
 'use strict';
 var assert = require('assert');
 
-describe('codeigniter generator', function () {
+describe('restful-ci generator', function () {
   it('can be imported without blowing up', function () {
     var app = require('../app');
     assert(app !== undefined);
