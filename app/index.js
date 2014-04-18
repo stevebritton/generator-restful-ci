082ddd833d889829787f5a0fc10ab521004a8db5
Steven Britton <stvnbritton@gmail.com>
2014-04-18 17:09:52 -0400

    various

--- app/index.js
+++ app/index.js
@@ -1,15 +1,29 @@
-'use strict';
-var util = require('util');
-var path = require('path');
-var spawn = require('child_process').spawn;
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
+//'use strict';
+
+
+// Requirements
+var util = require('util'),
+    path = require('path'),
+    fs = require('fs'),
+    yeoman = require('yeoman-generator'),
+    wrench = require('wrench'),
+    chalk = require('chalk'),
+    mkdirp = require('mkdirp'),
+    codeigniter = require('../util/codeigniter'),
+    splash = require('../util/splash'),
+    Logger = require('../util/log'),
+    Config = require('../util/config');
 
 
 
+// Export the module
+module.exports = Generator;
 
-var RestfulCiGenerator = module.exports = function RestfulCiGenerator(args, options, config) {
+// Extend the base generator
+function Generator(args, options, config) {
     yeoman.generators.Base.apply(this, arguments);
+
+
 
     // setup the test-framework property, Gruntfile template will need this
     this.testFramework = options['test-framework'] || 'mocha';
@@ -29,115 +43,171 @@ var RestfulCiGenerator = module.exports = function RestfulCiGenerator(args, opti
         }
     });
 
-    this.options = options;
+    // Log level option
+    this.option('log', {
+        desc: 'The log verbosity level: [ verbose | log | warn | error ]',
+        defaults: 'log',
+        alias: 'l',
+        name: 'level'
+    });
+
+    // Enable advanced features
+    this.option('advanced', {
+        desc: 'Makes advanced features available',
+        alias: 'a'
+    });
+
+
+    // Shortcut for --log=verbose
+    this.option('verbose', {
+        desc: 'Verbose logging',
+        alias: 'v'
+    });
+    if (this.options.verbose) {
+        this.options.log = 'verbose';
+    }
+
+    // Setup the logger
+    this.logger = Logger({
+        level: this.options.log
+    });
+
+    // Log the options
+    try {
+        this.logger.verbose('\nOptions: ' + JSON.stringify(this.options, null, '  '));
+    } catch (e) {
+        // This is here because when a generator is run by selecting it after running `yo`,
+        // the options is a circular data structure, causing an error when converting to json.
+        // Verbose cannot be called this way, so there is no need to log anything.
+    }
+
+    // Load the config files
+    this.conf = new Config();
+
+    //this.options = options;
 
     this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
 
-util.inherits(RestfulCiGenerator, yeoman.generators.Base);
+util.inherits(Generator, yeoman.generators.Base);
 
-RestfulCiGenerator.prototype.askFor = function askFor() {
 
-    var done = this.async();
+Generator.prototype.whatCanYoemanDoForYou = function() {
+
+    var done = this.async(),
+        me = this;
 
     // welcome message
-    if (!this.options['skip-welcome-message']) {
-        this.log(this.yeoman);
-        this.log(chalk.magenta('You\'re using the fantastic Restful Codeigniter generator. Out of the box I include Codeigniter, jQuery, and a Gruntfile.js to build your app.'));
+    this.logger.log(splash.rci, {
+        logPrefix: ''
+    });
+
+    getInput();
+
+    function getInput() {
+        me.prompt(require('./prompts')(me.options.advanced, me.conf.get()), function(input) {
+            me.prompt([{
+                message: 'Does this all look correct?',
+                name: 'confirm',
+                type: 'confirm'
+            }], function(i) {
+                if (i.confirm) {
+
+
+                    me.logger.log('Installing CodeIgniter now...');
+
+                    //Place user defined CodeIgniter directory in the app folder
+                    //ToDo: create gloabl variable and set this in the Util/CodeIgniter file
+                    input.ciDir = 'app/' + input.ciDir
+
+                    //Get CodeIgniter files
+                    //ToDo: pull down from the interwebs
+                    codeigniter.installCodeIgniter(me, input);
+
+
+                    var features = input.features;
+
+                    function hasFeature(feat) {
+                        return features.indexOf(feat) !== -1;
+                    }
+
+                    //ToDo: automate this based on what's being promted
+                    me.includeCompass = hasFeature('includeCompass');
+                    me.includeBootstrap = hasFeature('includeBootstrap');
+                    me.includeFontAwesome = hasFeature('includeFontAwesome');
+                    me.includeModernizr = hasFeature('includeModernizr');
+
+
+                    // Save the users input
+                    me.conf.set(input);
+
+                    me.logger.verbose('User Input: ' + JSON.stringify(me.conf.get(), null, '  '));
+                    me.logger.log(splash.go, {
+                        logPrefix: ''
+                    });
+                    done();
+
+                    me.logger.log('YO, CodeIgniter is now installed!');
+
+                } else {
+                    console.log();
+                    getInput();
+                }
+            });
+        });
     }
-
-
-    var prompts = [{
-        type: 'checkbox',
-        name: 'features',
-        message: 'What more would you like?',
-        choices: [{
-            name: 'Bootstrap',
-            value: 'includeBootstrap',
-            checked: true
-        }, {
-            name: 'Sass with Compass',
-            value: 'includeCompass',
-            checked: false
-        }, {
-            name: 'Modernizr',
-            value: 'includeModernizr',
-            checked: false
-        }]
-    }];
-
-    this.prompt(prompts, function(answers) {
-        var features = answers.features;
-
-        function hasFeature(feat) {
-            return features.indexOf(feat) !== -1;
-        }
-
-        this.includeCompass = hasFeature('includeCompass');
-        this.includeBootstrap = hasFeature('includeBootstrap');
-        this.includeModernizr = hasFeature('includeModernizr');
-
-        done();
-    }.bind(this));
-
-
 };
 
+// Save settings to .restful-ci file
+Generator.prototype.saveDaMoFoSettings = function() {
+    this.logger.log('Writing .restful-ci file');
+    fs.writeFileSync('.restful-ci', JSON.stringify(this.conf.get(), null, '\t'));
+};
 
-RestfulCiGenerator.prototype.gruntfile = function gruntfile() {
+Generator.prototype.getMaGruntfile = function() {
     this.template('Gruntfile.js');
 };
 
-RestfulCiGenerator.prototype.packageJSON = function packageJSON() {
+Generator.prototype.andMyPackage = function() {
     this.template('_package.json', 'package.json');
 };
 
-RestfulCiGenerator.prototype.git = function git() {
+Generator.prototype.alsoGetGit = function() {
     this.copy('gitignore', '.gitignore');
     this.copy('gitattributes', '.gitattributes');
 };
 
-RestfulCiGenerator.prototype.bower = function bower() {
+Generator.prototype.bowDownTOBower = function() {
     this.copy('bowerrc', '.bowerrc');
     this.copy('_bower.json', 'bower.json');
 };
 
-RestfulCiGenerator.prototype.jshint = function jshint() {
+Generator.prototype.jshintDatShit = function() {
     this.copy('jshintrc', '.jshintrc');
 };
 
-RestfulCiGenerator.prototype.editorConfig = function editorConfig() {
+Generator.prototype.editorConfig = function() {
     this.copy('editorconfig', '.editorconfig');
 };
 
-RestfulCiGenerator.prototype.h5bp = function h5bp() {
+
+Generator.prototype.h5bp = function() {
     this.copy('favicon.ico', 'app/favicon.ico');
     this.copy('404.html', 'app/404.html');
     this.copy('robots.txt', 'app/robots.txt');
     this.copy('htaccess', 'app/.htaccess');
 };
 
-RestfulCiGenerator.prototype.Codeigniter = function Codeigniter() {
-    this.directory('codeigniter/application', 'app/application');
-    this.directory('codeigniter/system', 'app/system');
-    this.copy('codeigniter/index.php', 'app/index.php');
-};
-
-RestfulCiGenerator.prototype.mainStylesheet = function mainStylesheet() {
-    var css = 'main.' + (this.includeCompass ? 's' : '') + 'css';
-    this.copy(css, 'app/web/styles/' + css);
-};
-
-RestfulCiGenerator.prototype.writeIndex = function writeIndex() {
+Generator.prototype.createIndexFile = function() {
 
     this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
     this.indexFile = this.engine(this.indexFile, this);
 
     // wire Bootstrap plugins
     if (this.includeBootstrap) {
-        var bs = '<?php echo(base_url) ?>/bower_components/bootstrap' + (this.includeCompass ? '-sass-official/vendor/assets/javascripts/bootstrap/' : '/js/');
-        this.indexFile = this.appendScripts(this.indexFile, 'scripts/plugins.js', [
+        var bs = 'bower_components/bootstrap' + (this.includeCompass ? '-sass-official/vendor/assets/javascripts/bootstrap/' : '/js/');
+        this.indexFile = this.appendScripts(this.indexFile, 'web/scripts/plugins.js', [
             bs + 'affix.js',
             bs + 'alert.js',
             bs + 'dropdown.js',
@@ -162,8 +232,12 @@ RestfulCiGenerator.prototype.writeIndex = function writeIndex() {
     });
 };
 
+Generator.prototype.mainStylesheet = function() {
+    var css = 'main.' + (this.includeCompass ? 's' : '') + 'css';
+    this.copy(css, 'app/web/styles/' + css);
+};
 
-RestfulCiGenerator.prototype.app = function app() {
+Generator.prototype.app = function() {
 
     this.mkdir('app');
     this.mkdir('app/web');
@@ -172,7 +246,7 @@ RestfulCiGenerator.prototype.app = function app() {
     this.mkdir('app/web/styles');
     this.mkdir('app/web/images');
 
-    this.write('app/application/views/index.php', this.indexFile);
+    this.write('app/index.html', this.indexFile);
 
     if (this.coffee) {
         this.write(
@@ -184,7 +258,7 @@ RestfulCiGenerator.prototype.app = function app() {
     }
 };
 
-RestfulCiGenerator.prototype.install = function() {
+Generator.prototype.install = function() {
     if (this.options['skip-install']) {
         return;
     }
@@ -194,5 +268,17 @@ RestfulCiGenerator.prototype.install = function() {
         skipMessage: this.options['skip-install-message'],
         skipInstall: this.options['skip-install'],
         callback: done
+    });
+};
+
+
+
+// All done
+Generator.prototype.aboutDamnTime = function() {
+    this.logger.log(chalk.bold.green('\nAll Done!!\n------------------------\n'), {
+        logPrefix: ''
+    });
+    this.logger.log('I tried my best to set things up, but I\'m only human right? **wink wink**\nSo, you should probably check your `config.php` to make sure all the settings work on your environment.', {
+        logPrefix: ''
     });
 };
